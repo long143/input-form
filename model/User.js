f0bca3254a336cd4246e4bc5fe968f1f2d5aebaf
long <longpmhd@gmail.com>
2023-05-08 21:12:02 +0700

    bt

---
+++ model/User.js
@@ -0,0 +1,20 @@
+class User {
+    id;
+    name;
+    username;
+    email;
+    password;
+    roles;
+   avatar;
+
+    constructor(id, name, username, email, password) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = 'user';
+        this.avatar = avatar;
+    }
+
+}
